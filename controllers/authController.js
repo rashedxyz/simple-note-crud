df7b1536c142a93684f76b646be3e2b41671abec
rashedxyz <rashed.webdev@gmail.com>
2021-04-16 22:52:08 +0600

    working on authentication

---
+++ controllers/authController.js
@@ -0,0 +1,11 @@
+module.exports.signupGet = (req, res) => {
+  res.render("signup");
+};
+
+module.exports.loginGet = (req, res) => {
+  res.render("login");
+};
+
+module.exports.signupPost = (req, res) => {};
+
+module.exports.loginPost = (req, res) => {};
